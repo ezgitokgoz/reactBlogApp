7c9fc60750e51d14117dd59cf8f8c6c201dc69fd
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2022-08-20 20:17:24 +0300

    writePage

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,22 @@
+import "./write.css"
+
+export default function Write() {
+  return (
+    <div className='write'>
+        <img className ='writeImg'src="https://images.pexels.com/photos/1371527/pexels-photo-1371527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <form className="writeForm">
+            <div className="writeFormGroup">
+               <label htmlFor="fileInput">
+                  <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileinput" style={{display:"none"}}/>
+                <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+    </div>
+  )
+}
